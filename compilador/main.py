2c84e13b30c0fcc7ecbd4b761ff0d468df602716
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-02-22 18:15:12 -0300

    reformat code

--- compilador/main.py
+++ compilador/main.py
@@ -1,35 +1,37 @@
 import sys
 
+
 def resolve_operacao(string):
 
-    string = string.replace(' ', '')
+    string = string.replace(" ", "")
     numbers = []
     symbols = []
-    
+
     for i in range(len(string)):
-        if string[i] == '+' or string[i] == '-':
-            if (string[i-1] == '+' or string[i-1] == '-') or (string[i+1] == '+' or string[i+1] == '-'):
+        if string[i] == "+" or string[i] == "-":
+            if (string[i - 1] == "+" or string[i - 1] == "-") or (
+                string[i + 1] == "+" or string[i + 1] == "-"
+            ):
                 print("Erro: sintaxe inválida")
                 return
 
             symbols.append(string[i])
-            string = string[:i] + ' ' + string[i+1:] 
-        
-    numbers = [int(i) for i in string.strip().split(' ')]
-    
+            string = string[:i] + " " + string[i + 1 :]
+
+    numbers = [int(i) for i in string.strip().split(" ")]
+
     if len(numbers) == len(symbols):
-        if symbols[0] == '+':
+        if symbols[0] == "+":
             resultado = numbers[0]
-        elif symbols[0] == '-':
-            resultado = - numbers[0]
+        elif symbols[0] == "-":
+            resultado = -numbers[0]
         del symbols[0]
     else:
         resultado = numbers[0]
-
     for j in range(1, len(numbers)):
-        if symbols[j-1] == '+':
+        if symbols[j - 1] == "+":
             resultado += numbers[j]
-        if symbols[j-1] == '-':
+        if symbols[j - 1] == "-":
             resultado -= numbers[j]
 
     print(resultado)
@@ -43,7 +45,3 @@ if __name__ == "__main__":
 
     for i in args:
         resolve_operacao(i)
-
-           
-
-
