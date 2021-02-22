0a01a8a4a958f005eaf1ea75ecca26185887bc1d
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-02-22 19:51:55 -0300

    classe

--- compilador/main.py
+++ compilador/main.py
@@ -1,40 +1,51 @@
 import sys
 
 
-def resolve_operacao(string):
+class Compiler:
+    def __init__(self, initString):
+        self.string = initString
+        self.numbers = []
+        self.symbols = []
+        self.result = None
 
-    string = string.replace(" ", "")
-    numbers = []
-    symbols = []
+    def compile_code(self):
+        self.string = self.string.replace(" ", "")
 
-    for i in range(len(string)):
-        if string[i] == "+" or string[i] == "-":
-            if (string[i - 1] == "+" or string[i - 1] == "-") or (
-                string[i + 1] == "+" or string[i + 1] == "-"
-            ):
-                print("Erro: sintaxe inválida")
-                return
+        for i in range(len(self.string)):
+            if self.string[i] == "+" or self.string[i] == "-":
+                if (self.string[i - 1] == "+" or self.string[i - 1] == "-") or (
+                    self.string[i + 1] == "+" or self.string[i + 1] == "-"
+                ):
+                    print("Erro: sintaxe inválida")
+                    return False
 
-            symbols.append(string[i])
-            string = string[:i] + " " + string[i + 1 :]
+                self.symbols.append(self.string[i])
+                self.string = self.string[:i] + " " + self.string[i + 1 :]
 
-    numbers = [int(i) for i in string.strip().split(" ")]
+        self.numbers = [int(i) for i in self.string.strip().split(" ")]
+        return True
 
-    if len(numbers) == len(symbols):
-        if symbols[0] == "+":
-            resultado = numbers[0]
-        elif symbols[0] == "-":
-            resultado = -numbers[0]
-        del symbols[0]
-    else:
-        resultado = numbers[0]
-    for j in range(1, len(numbers)):
-        if symbols[j - 1] == "+":
-            resultado += numbers[j]
-        if symbols[j - 1] == "-":
-            resultado -= numbers[j]
+    def calculate(self):
+        if len(self.numbers) == len(self.symbols):
+            if self.symbols[0] == "+":
+                self.result = self.numbers[0]
+            elif self.symbols[0] == "-":
+                self.result = -self.numbers[0]
+            del self.symbols[0]
+        else:
+            self.result = self.numbers[0]
 
-    print(resultado)
+        for j in range(1, len(self.numbers)):
+            if self.symbols[j - 1] == "+":
+                self.result += self.numbers[j]
+            if self.symbols[j - 1] == "-":
+                self.result -= self.numbers[j]
+
+    def execute(self):
+        can_calculate = self.compile_code()
+        if can_calculate:
+            self.calculate()
+            print(self.result)
 
 
 if __name__ == "__main__":
@@ -44,4 +55,4 @@ if __name__ == "__main__":
         args.append(sys.argv[i])
 
     for i in args:
-        resolve_operacao(i)
+        Compiler(i).execute()
