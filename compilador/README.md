9abe323f7f4eaeee556ffee6fdfa14c17a92def5
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-02-24 19:03:17 -0300

    Better code, using regex to get both numbers and operands

--- compilador/README.md
+++ compilador/README.md
@@ -2,4 +2,4 @@
 
 v0.1.1: soma e subtracao simples por meio de uma string na linha de comando
 
-```python main.py '1+2' '3-2' '11+22-33' '789 +345 - 123' '7- - 1' '-2-3' '+4+88'```
+```python main.py '1+2' '3-2' '11+22-33' '789 +345 - 123' '-2-3' '+4+88' '3 4' '10 4' '10+' '-5' '10++10' '7- - 1'```
--- compilador/main.py
+++ compilador/main.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 
 class Compiler:
@@ -6,34 +7,59 @@ class Compiler:
         self.string = initString
         self.numbers = []
         self.symbols = []
-        self.result = None
+        self.result = 0
 
-    def compile_code(self):
+    def getNumbers(self):
+        self.numbers = [int(i) for i in re.findall(r"\d+", self.string)]
+        # print(self.numbers)
+
+    def getSymbols(self):
+        self.symbols = re.findall(r"\D", self.string)
+        # print(re.findall(r"\D", self.string))
+
+    def checkSymbols(self):
+        accepted_symbols = ["+", "-"]
+        self.symbols = list(filter(lambda x: x in accepted_symbols, self.symbols))
+        # print(self.symbols)
+
+    def insuficientOperands(self):
+        if len(self.symbols) <= len(self.numbers) - 2:
+            # 3 4 --> 2 numbers 0 symbols
+            # 2+4 4 --> 3 numbers 1 symbol
+            print("Error: Insufficient operands to perform operation")
+            return True
+        return False
+
+    def isValidString(self):
         self.string = self.string.replace(" ", "")
+        try:
+            for i in range(len(self.string)):
+                if self.string[i] == "+" or self.string[i] == "-":
+                    if self.string[i + 1] == "+" or self.string[i + 1] == "-":
+                        print("Error: invalid syntax")
+                        return False
+        except:
+            # example: 10+3+4-
+            print("Error: invalid operand position")
+            return False
 
-        for i in range(len(self.string)):
-            if self.string[i] == "+" or self.string[i] == "-":
-                if (self.string[i - 1] == "+" or self.string[i - 1] == "-") or (
-                    self.string[i + 1] == "+" or self.string[i + 1] == "-"
-                ):
-                    print("Erro: sintaxe inválida")
-                    return False
-
-                self.symbols.append(self.string[i])
-                self.string = self.string[:i] + " " + self.string[i + 1 :]
-
-        self.numbers = [int(i) for i in self.string.strip().split(" ")]
         return True
 
+    def isValidEquation(self):
+        if len(self.numbers) > 1:
+            return True
+        print("Error: cannot calculate equation with one number")
+        return False
+
     def calculate(self):
-        if len(self.numbers) == len(self.symbols):
-            if self.symbols[0] == "+":
-                self.result = self.numbers[0]
-            elif self.symbols[0] == "-":
-                self.result = -self.numbers[0]
-            del self.symbols[0]
+        if len(self.symbols) != len(self.numbers):
+            self.result += self.numbers[0]
         else:
-            self.result = self.numbers[0]
+            if self.symbols[0] == "+":
+                self.result += self.numbers[0]
+            elif self.symbols[0] == "-":
+                self.result -= self.numbers[0]
+            del self.symbols[0]
 
         for j in range(1, len(self.numbers)):
             if self.symbols[j - 1] == "+":
@@ -41,11 +67,18 @@ class Compiler:
             if self.symbols[j - 1] == "-":
                 self.result -= self.numbers[j]
 
+        print(self.result)
+
     def execute(self):
-        can_calculate = self.compile_code()
-        if can_calculate:
+        self.getNumbers()
+        self.getSymbols()
+        self.checkSymbols()
+        if (
+            self.isValidEquation()
+            and not self.insuficientOperands()
+            and self.isValidString()
+        ):
             self.calculate()
-            print(self.result)
 
 
 if __name__ == "__main__":
