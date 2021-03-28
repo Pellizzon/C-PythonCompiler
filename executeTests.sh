5f332c3c664409112c96f5d27e165da56113cbfb
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-03-27 22:07:30 -0300

    better error message on test

--- executeTests.sh
+++ executeTests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' 'err' '49')
+R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' 'ValueError' '49')
 files=(./tests/*)
 N_tests=${#R[@]}
 
@@ -8,7 +8,7 @@ do
     A=$(python3 main.py ${files[$i]} 2>&1)
     if ! [[ $A == ${R[$i]} ]]; then # if not equal to number in list
         if ! [[ $A =~ ValueError ]]; then # if regex doesn't match
-            echo "Failure on ${files[$i]}" 
+            echo "Failure on ${files[$i]}. Expected ${R[$i]}, but got $A." 
             exit; 
         fi
     fi
