c3d137d1a313ed0965ad4e049a42683f90093a85
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-05-15 12:14:11 -0300

    roteiro 07

--- test.c
+++ test.c
@@ -1,11 +1,32 @@
 {
-    int i;
-    i = --3;
-    while (i < 5)
-    {
-        i = i + 1;
-        println(i);
-    }
+    int x;
+    bool y;
+    string z;
+    x = 0;
+    y = 3 > 2;
+    z = "b";
+    println(x);
+    println(x + y);
+    println(z);
+    println(z == "a");
 
-    println(!"a");
+    while (x < 10 && y)
+    {
+        x = x + 1;
+        println(x + y);
+        if (x == 4 && y)
+        {
+            y = !y;
+            println("y inverteu valor");
+        }
+    }
+    println("saiu do while");
+
+    /*ERRO:*/
+    println(z > x);
+    println(z == y);
+    while (z)
+    {
+        println(z);
+    }
 }
